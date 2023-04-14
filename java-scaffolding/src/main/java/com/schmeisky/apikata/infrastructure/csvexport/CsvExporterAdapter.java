ad8e16557fdbf7a632396f68fe9e842de1028193
Robert Gurol <robert.gurol@ebcont.com>
2023-04-14 22:36:40 +0200

    implement CsvExporterAdapter
    shameless copy + adjustments from
    Christoph's and my old code

---
+++ java-scaffolding/src/main/java/com/schmeisky/apikata/infrastructure/csvexport/CsvExporterAdapter.java
@@ -0,0 +1,69 @@
+package com.schmeisky.apikata.infrastructure.csvexport;
+
+import com.schmeisky.apikata.business.ExporterPort;
+import com.schmeisky.apikata.business.WeatherData;
+import com.schmeisky.apikata.config.GlobalConfig;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Clock;
+import java.time.Instant;
+import java.util.List;
+
+public class CsvExporterAdapter implements ExporterPort {
+
+    private static final CSVFormat CSV_FORMAT = CSVFormat.DEFAULT
+            .builder()
+            .setHeader("id", "name", "date", "time", "temperature", "pressure", "wind_direction")
+            .setRecordSeparator("\n")
+            .build();
+
+    private final Path outputDir;
+    private final Clock clock;
+
+    public CsvExporterAdapter() {
+        this(GlobalConfig.APP_OUTPUT_DIR, Clock.systemDefaultZone());
+    }
+
+    public CsvExporterAdapter(Path outputDir, Clock clock) {
+        this.outputDir = outputDir;
+        this.clock = clock;
+    }
+
+    @Override
+    public void exportToNewFile(List<WeatherData> data) {
+        String fileName = "weather_data_" + Instant.now(clock).toString() + ".csv";
+        try {
+            Path destination = Files.createFile(outputDir.resolve(fileName));
+            exportThrowing(data, destination);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void exportThrowing(List<WeatherData> data, Path destination) throws IOException {
+        StringWriter writer = new StringWriter();
+        try (CSVPrinter printer = new CSVPrinter(writer, CSV_FORMAT)) {
+            data.forEach(entity -> print(printer, entity));
+        }
+
+        try (OutputStream outputStream = Files.newOutputStream(destination)) {
+            IOUtils.write(writer.toString(), outputStream, StandardCharsets.UTF_8);
+        }
+    }
+
+    private static void print(CSVPrinter printer, WeatherData entity) {
+        try {
+            printer.printRecord(entity.getStationId(), entity.getStationName(), entity.getDate(), entity.getTime(), entity.getAirTemperature(), entity.getAtmosphericPressure(), entity.getWindDirection());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ java-scaffolding/src/test/java/com/schmeisky/apikata/infrastructure/csvexport/CsvExporterAdapterTest.java
@@ -0,0 +1,43 @@
+package com.schmeisky.apikata.infrastructure.csvexport;
+
+import com.schmeisky.apikata.business.ExporterPort;
+import com.schmeisky.apikata.business.WeatherData;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.nio.file.Path;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CsvExporterAdapterTest {
+
+    private static final Instant TEST_TIME = Instant.parse("2023-04-14T22:22:22.00+02:00");
+    private static final Clock TEST_CLOCK = Clock.fixed(TEST_TIME, ZoneId.of("Europe/Berlin"));
+
+    @Test
+    public void export_writesDataAsCsv(@TempDir Path tempDir) throws Exception {
+        List<WeatherData> inputData = aWeatherDataList();
+        ExporterPort csvExporter = new CsvExporterAdapter(tempDir, TEST_CLOCK);
+
+        csvExporter.exportToNewFile(inputData);
+        Path outputPath = tempDir.resolve("weather_data_2023-04-14T20:22:22Z.csv");
+        assertThat(outputPath).exists();
+        assertThat(outputPath).content().isEqualTo(
+                "id,name,date,time,temperature,pressure,wind_direction\n" +
+                        "1,Vienna,2023-04-14,13:37:00,20,1000,N\n" +
+                        "2,Potsdam,2023-04-13,00:23:42,\"-11,7\",999,S\n" +
+                        "3,Prague,2023-04-13,00:23:42,\"-11,7\",999,\n");
+    }
+
+    private List<WeatherData> aWeatherDataList() {
+        WeatherData item1 = new WeatherData(1, "Vienna", "2023-04-14", "13:37:00", "20", "1000", "N");
+        WeatherData item2 = new WeatherData(2, "Potsdam", "2023-04-13", "00:23:42", "-11,7", "999", "S");
+        WeatherData item3 = new WeatherData(3, "Prague", "2023-04-13", "00:23:42", "-11,7", "999", null);
+        return List.of(item1, item2, item3);
+    }
+
+}
