fbb7576a9f154c2c7ab09370eace5f63f0bff395
Robert Gurol <robert.gurol@ebcont.com>
2023-04-14 22:20:03 +0200

    refactor: avoid helper code (do all via ModelMapper)

--- java-scaffolding/src/main/java/com/schmeisky/apikata/infrastructure/apifetch/ApiWeatherDataMapper.java
+++ java-scaffolding/src/main/java/com/schmeisky/apikata/infrastructure/apifetch/ApiWeatherDataMapper.java
@@ -3,28 +3,44 @@ package com.schmeisky.apikata.infrastructure.apifetch;
 import com.schmeisky.apikata.business.WeatherData;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MappingContext;
 
 public class ApiWeatherDataMapper {
 
     private final ModelMapper modelMapper;
 
     public ApiWeatherDataMapper() {
-        modelMapper = new ModelMapper();
+        modelMapper = createAndConfigureModelMapper();
+    }
+
+    private ModelMapper createAndConfigureModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         modelMapper.typeMap(ApiWeatherData.class, WeatherData.class)
                 .addMappings(mapper -> mapper.skip(WeatherData::setTime))
                 .addMappings(mapper -> mapper.skip(WeatherData::setDate))
-        ;
+                .addMappings(mapper -> mapper.using(this::timeStampStringToDateStringConverter)
+                        .map(ApiWeatherData::getDateTime, WeatherData::setDate))
+                .addMappings(mapper -> mapper.using(this::timeStampStringToTimeStringConverter)
+                        .map(ApiWeatherData::getDateTime, WeatherData::setTime));
         modelMapper.validate();
+        return modelMapper;
+    }
+
+    private String timeStampStringToDateStringConverter(MappingContext<Object, Object> context) {
+        String[] splitTimeStamp = context.getSource().toString().split(" ");
+        return splitTimeStamp[0];
+    }
+
+    private String timeStampStringToTimeStringConverter(MappingContext<Object, Object> context) {
+        String[] splitTimeStamp = context.getSource().toString().split(" ");
+        if (splitTimeStamp.length > 1) {
+            return splitTimeStamp[1];
+        }
+        return null;
     }
 
     public WeatherData fromApiWeatherData(ApiWeatherData input) {
-        WeatherData result = modelMapper.map(input, WeatherData.class);
-        String[] splitTimeStamp = input.getDateTime().split(" ");
-        if (splitTimeStamp.length > 1) {
-            result.setDate(splitTimeStamp[0]);
-            result.setTime(splitTimeStamp[1]);
-        }
-        return result;
+        return modelMapper.map(input, WeatherData.class);
     }
 }
